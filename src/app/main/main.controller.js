228954db0809fa9cf1f16d4d3b60fae333e62b53
Graham Heath <graham.p.heath@gmail.com>
2015-04-25 00:21:28 -0500

    first working pose socket connection, firefly lights

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,8 @@ angular.module('mysteryProject')  .controller('MainCtrl', function ($scope) {
 
   var light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 0, 0, 1 );
+    $scope.scene.push( light );
+
   //var cube = new THREE.Mesh( geometry, material );
 
 
@@ -45,27 +47,81 @@ angular.module('mysteryProject')  .controller('MainCtrl', function ($scope) {
 
 
   var lights = [
-    {color:0x4040ff}, //001
-    {color:0x40ff40}, //010
-    {color:0x40ffff}, //011
-    {color:0xff4040}, //100
-    {color:0xff40ff}, //101
-    {color:0xffff00}, //110
-    {color:0xffffff}  //111
+    {
+      color:0x4040ff,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //001
+    {
+      color:0x40ff40,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //010
+    {
+      color:0x40ffff,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //011
+    {
+      color:0xff4040,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //100
+    {
+      color:0xff40ff,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //101
+    {
+      color:0xffff00,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }, //110
+    {
+      color:0xffffff,
+      seeds: {
+       x:Math.random(),
+       y:Math.random(),
+       z:Math.random()
+      }
+    }  //111
   ];
-
+  $scope.flies = [];
   for (var i = lights.length - 1; i >= 0; i--) {
     var spot = new THREE.PointLight( lights[i].color );
 
     spot.position.z = (Math.random() * 5) - 2.5;
     spot.position.x = (Math.random() * 5) - 2.5;
     spot.position.y = (Math.random() * 5) - 2.5;
-
+    var mesh = new THREE.Mesh( geometry, red );
+    mesh.scale.x = 0.2;
+    mesh.scale.y = 0.2;
+    mesh.scale.z = 0.2;
+    $scope.flies.push({spot:spot, mesh: mesh});
     $scope.scene.push( spot );
+    $scope.scene.push( mesh );
+
   }
 
 
-  $scope.scene.push( light );
   // $scope.scene.push( cube );
 
 
@@ -73,7 +129,18 @@ angular.module('mysteryProject')  .controller('MainCtrl', function ($scope) {
   var seedY = Math.random();
 
   var worldRenderLoop = function () {
+    for (var i = 0; i <  $scope.flies.length; i++){
+      var light =  $scope.flies[i].spot;
 
+      var mesh =  $scope.flies[i].mesh;
+      var max = 10;
+      light.position.x = (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.x)) - (max/2);
+      light.position.y = max + (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.y)) - (max/2);
+      light.position.z = (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.z)) - (max/2);
+      mesh.position.x = (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.x)) - (max/2);
+      mesh.position.y = max + (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.y)) - (max/2);
+      mesh.position.z = (max * noise.perlin2(Date.now() / 1000, lights[i].seeds.z)) - (max/2);
+    }
   }
 
   var cubeRenderLoop = function () {
@@ -98,5 +165,12 @@ angular.module('mysteryProject')  .controller('MainCtrl', function ($scope) {
   $scope.scene.renderLoop = cubeRenderLoop;
   $scope.scene.renderLoop = worldRenderLoop;
        
+
+  var socket = io('http://localhost:3001');
+
+  socket.on('location', function(pose) {
+    $scope.pose = pose;
+    $scope.$broadcast('location',pose);
+  });
 
 });
--- src/components/zthree/zthree.directive.js
+++ src/components/zthree/zthree.directive.js
@@ -5,7 +5,8 @@ angular.module('mysteryProject')
 	  return {
 	  	restrict: 'E',
 	  	scope:{
-	  		scene:"="
+	  		scene:'=',
+	  		pose:'='
 	  	},
 	    template: '<canvas></canvas>',
 	    link: function (scope, element, attrs) {
@@ -14,7 +15,13 @@ angular.module('mysteryProject')
   			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 		    var renderer = new THREE.WebGLRenderer({canvas:element[0].childNodes[0]});
 		    renderer.setSize( window.innerWidth - 50, window.innerHeight - 20 );
-         camera.position.z = 5;
+         camera.position.z = 8;
+        
+        scope.$on('location', function(event,pose) {
+         	camera.rotation.z = pose.z;
+         	camera.rotation.x = pose.x;
+         	camera.rotation.y = pose.y;
+        });
 
 		    for(var i = 0; i < scope.scene.length; i++){
 		    	scene.add(scope.scene[i]);
@@ -23,6 +30,11 @@ angular.module('mysteryProject')
 		    function render() {
 		    	requestAnimationFrame( render );
 		      renderer.render( scene, camera );
+		      
+		      if (scope.pose !== undefined) {
+		      	debugger;
+		      }
+
 		      if (scope.scene.renderLoop !== undefined) {
 		      	if (typeof scope.scene.renderLoop === 'function') {
 		      		scope.scene.renderLoop();
