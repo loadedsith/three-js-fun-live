0431d94818aeae95cf52e795b0f5c66c7eda765f
Graham Heath <graham.p.heath@gmail.com>
2015-05-15 20:10:13 -0500

    enable mozVR, clean up

--- src/components/zthree/zthree.directive.js
+++ src/components/zthree/zthree.directive.js
@@ -19,8 +19,14 @@ angular.module('mysteryProject')
 				var camera = scope.camera;
 		    var renderer = new THREE.WebGLRenderer({canvas:element[0].childNodes[0]});
 		    renderer.setSize( window.innerWidth - 0, window.innerHeight - 0);
-        
-        // var vrrenderer = new THREE.VRRenderer(renderer, vrHMD);
+
+    		var controls = new THREE.VRControls( camera );
+
+				/*
+				Apply VR stereo rendering to renderer
+				*/
+				var effect = new THREE.VREffect( renderer );
+				effect.setSize( window.innerWidth, window.innerHeight );
 
         scope.$on('location', function(event,pose) {
          	camera.rotation.z = pose.z;
@@ -40,12 +46,25 @@ angular.module('mysteryProject')
   			});
 
 		    function render() {
+
+					/*
+					Update VR headset position and apply to camera.
+					*/
+					controls.update();
+
+					/*
+					Render the scene through the VREffect.
+					*/
+					effect.render( scene, camera );
+
 		      scene.simulate(); // run physics
 
 					scene.setGravity(scope.gravity);
 
+
+
 		    	requestAnimationFrame( render );
-		      renderer.render( scene, camera );
+		      //renderer.render( scene, camera );
 
 		      if (scope.pose !== undefined) {
 		      	debugger;
@@ -58,6 +77,42 @@ angular.module('mysteryProject')
 		      }
 		    }
 		    render();
+
+
+				/*
+				Listen for double click event to enter full-screen VR mode
+				*/
+				document.body.addEventListener( 'dblclick', function() {
+					effect.setFullScreen( true );
+				});
+
+				/*
+				Listen for keyboard event and zero positional sensor on appropriate keypress.
+				*/
+				function onkey(event) {
+			    event.preventDefault();
+
+			    if (event.keyCode == 90) { // z
+			    	controls.zeroSensor();
+			    }
+			  };
+
+			  window.addEventListener("keydown", onkey, true);
+
+
+
+				/*
+				Handle window resizes
+				*/
+				function onWindowResize() {
+					camera.aspect = window.innerWidth / window.innerHeight;
+					camera.updateProjectionMatrix();
+
+					effect.setSize( window.innerWidth, window.innerHeight );
+				}
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
 	    }
 	  };
 	});
